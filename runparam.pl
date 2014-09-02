45f44db1af092bd6d967697da5c4415e4c37efa1
Wesley Erickson <inflamedspirit@gmail.com>
2014-09-02 14:42:38 -0700

    Added some perl launch scripts.

---
+++ runparam.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+
+use strict;
+
+# Output files
+our $stdoutfile;
+our $stderrfile;
+
+# Declare Parameters to be loaded:
+our $rabi_frequency;
+our $detuning;
+our $wavenumber;
+our $decay_rate;
+our $vx;
+our $vy;
+our $vz;
+our $tstep;
+our $tfinal;
+our $save_interval;
+our $seed;
+our $focalradius;
+our $focalthreshhold;
+our $motradius;
+
+# Load Parameters
+if(@ARGV != 1 ){
+    die "Needs parameter file as argument.";
+}
+my $paramfile = $ARGV[0];
+require $paramfile;
+
+# Launch Mot
+system("time ./mot $rabi_frequency $detuning $wavenumber $decay_rate $vx $vy $vz $focalradius $focalthreshhold $motradius $tstep $tfinal $save_interval $seed 1> $stdoutfile 2> $stderrfile ");
